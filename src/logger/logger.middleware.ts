99543e0361a9e971446f0c5f22698873bb538382
Zhiznevski <W.heisenberggg@gmail.com>
2025-12-14 21:30:43 +0100

    refactor: middleware minor changes

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -7,13 +7,13 @@ export class LoggerMiddleware implements NestMiddleware {
   constructor(private readonly loggingService: LoggingService) {}
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { method, originalUrl, query } = request;
+    const { method, originalUrl, query, body } = request;
 
     response.on('finish', () => {
       const { statusCode } = response;
 
       this.loggingService.log(
-        `method: ${method}, url: ${originalUrl}, statusCode: ${statusCode}, query: ${JSON.stringify(query)}, body: ${JSON.stringify(request.body)}`,
+        `method: ${method}, url: ${originalUrl}, statusCode: ${statusCode}, query: ${JSON.stringify(query)}, body: ${JSON.stringify(body)}`,
       );
     });
 
